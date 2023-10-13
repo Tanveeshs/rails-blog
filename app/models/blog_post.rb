992fab052337c72231e80d86da070be32ab2726a
Tanveesh <Tanveeshs@gmail.com>
2023-10-12 18:40:11 -0700

    Changes

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,4 +1,21 @@
 class BlogPost < ApplicationRecord
-  validates :title,presence: true
-  validates :body,presence: true
+  validates :title, presence: true
+  validates :body, presence: true
+
+  scope :sorted, -> { order(published_at: :desc, updated_at: :desc) }
+  scope :published, -> { where("published_at<=?", Time.current) }
+  scope :schedules, -> { where("published_at>?", Time.current) }
+  scope :draft, -> { where(published_at: nil) }
+
+  def draft?
+    published_at.nil?
+  end
+
+  def published?
+    published_at? && published_at <= Time.current
+  end
+
+  def scheduled?
+    published_at? && published_at > Time.current
+  end
 end
